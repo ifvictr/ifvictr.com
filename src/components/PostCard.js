9fed09a8612e263bec64cd2bb63ae74dddbe1a0a
Victor Truong <me@ifvictr.com>
2019-03-13 23:20:05 -0700

    Use href for regular link

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -47,7 +47,7 @@ const PostCard = ({ data, ...props }) => {
     return (
         <Base {...props}>
             <Inner
-                to={`https://blog.ifvictr.com/${data.slug}`}
+                href={`https://blog.ifvictr.com/${data.slug}`}
                 style={{
                     backgroundImage: `linear-gradient(
                         24deg,
