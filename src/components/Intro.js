269fe0c386970d1f6dd997db1ed02e53ad71a6c8
Victor Truong <me@ifvictr.com>
2019-04-16 16:58:46 -0700

    Add service icons

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Box, Flex, Heading, Image, Text } from 'rebass'
+import { FaDollarSign, FaEnvelope, FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
+import { Box, Flex, Heading, Image, Link, Text } from 'rebass'
 import styled from 'styled-components'
 
 import Container from 'components/Container'
@@ -37,6 +38,37 @@ const Avatar = styled(Image).attrs({ width: 200 })`
     box-shadow: ${({ theme }) => theme.boxShadows[1]};
 `
 
+const Services = styled(Flex)`
+    a {
+        line-height: 0;
+    }
+    svg {
+        fill: currentColor;
+        filter: drop-shadow(0 1px 2px rgba(0, 0, 0, 0.376));
+        height: 24px;
+        transition: fill ${({ theme }) => theme.transition};
+        width: 24px;
+        will-change: fill;
+    }
+
+    svg:hover {
+        fill: ${({ theme }) => theme.colors.smoke};
+    }
+`
+
+const Service = ({ href, icon: Icon, ...props }) => (
+    <Link
+        target="_blank"
+        rel="noopener"
+        href={href}
+        mr={3}
+        children={<Icon />}
+        {...props}
+    />
+)
+
+const Separator = styled(Box).attrs({ mr: 2, children: '·' })``
+
 const Intro = props => (
     <Base alignItems="center" justifyContent="center" {...props}>
         <Container maxWidth={64} width={1} style={{ zIndex: 1 }}>
@@ -47,9 +79,17 @@ const Intro = props => (
                 <Container maxWidth={32} ml={3}>
                     <Heading fontSize={6}>Victor Truong</Heading>
                     <Heading fontSize={3} mb={2}>@ifvictr</Heading>
-                    <Text fontSize={3}>
+                    <Text fontSize={3} mb={3}>
                         Software developer, startup enthusiast, & neophiliac. Always building.
                     </Text>
+                    <Services>
+                        <Service href="https://github.com/ifvictr" icon={FaGithub} />
+                        <Service href="https://linkedin.com/in/ifvictr" icon={FaLinkedinIn} />
+                        <Service href="https://twitter.com/ifvictr2" icon={FaTwitter} />
+                        <Separator />
+                        <Service href="https://paypal.me/ifvictr" icon={FaDollarSign} />
+                        <Service href="mailto:me@ifvictr.com" icon={FaEnvelope} />
+                    </Services>
                 </Container>
             </Flex>
         </Container>
